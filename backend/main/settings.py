981fe7456375e3d0e31565423646263866810b37
Mvrouvne <marouanechaibat@gmail.com>
2024-12-07 16:45:39 +0100

    debug to false

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -15,7 +15,7 @@ config = Config(RepositoryEnv(join(BASE_DIR, '.env')))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
